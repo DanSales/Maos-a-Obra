c925b946100f94d894bc8060f4145795bff34393
Matheus <noronhamatheus07@gmail.com>
2018-07-11 22:32:18 -0300

    cadastro e login no sistema

---
+++ projeto-web/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->prefix('painel')->group(function () {
+    Route::get('/','Usuarios\PainelController@index');
+
+    //Routes - Todos os usuários de level:0(Cliente)
+    Route::middleware(['level:0'])->group(function () {
+    });
+    //Routes - Todos os usuários de level:1(Prestador)
+    Route::middleware(['level:1'])->group(function () {
+    });
+    //Routes - Todos os usuários de level:2(Admin)
+    Route::middleware(['level:2'])->group(function () {
+    });
+});
